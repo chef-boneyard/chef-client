e7a5977dfb93432b21c213d48e258a39615e0a1a
Ringo De Smet <ringo.de.smet@atriso.be>
2012-06-19 10:18:03 +0200

    More work...

--- recipes/service.rb
+++ recipes/service.rb
@@ -234,6 +234,10 @@ when "winsw"
     action :start
   end
 
+when "macosx"
+
+
+
 when "bsd"
   log "You specified service style 'bsd'. You will need to set up your rc.local file."
   log "Hint: chef-client -i #{node["chef_client"]["client_interval"]} -s #{node["chef_client"]["client_splay"]}"
