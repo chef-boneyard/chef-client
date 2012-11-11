f059ff34942ce515de07f5ec70798e7f1b1d261b
Guilhem Lettron <guilhem@lettron.fr>
2012-11-11 20:38:20 +0100

    Change simple quote to double quote

--- attributes/default.rb
+++ attributes/default.rb
@@ -96,8 +96,8 @@ else
   default["chef_client"]["backup_path"] = "/var/chef/backup"
 end
 
-if node['chef_packages']['chef']['version'] >= "10.14"
-  default['chef_client']['fork'] = true
+if node["chef_packages"]["chef"]["version"] >= "10.14"
+  default["chef_client"]["fork"] = true
 else
-  default['chef_client']['fork'] = false
+  default["chef_client"]["fork"] = false
 end
