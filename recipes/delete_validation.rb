f54882489e7a8962d39e4b46bae0be37e7d47417
Jeremiah Snapp <jeremiah.snapp@gmail.com>
2012-11-28 08:53:23 -0500

    disable delete_validation recipe if chef-server binary exists

--- recipes/delete_validation.rb
+++ recipes/delete_validation.rb
@@ -17,7 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-unless node['recipes'].include?("chef-server")
+unless system('which chef-server')
   file Chef::Config[:validation_key] do
     action :delete
     backup false
