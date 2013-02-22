20dd12d8eb74955a4e85f9aaed897c2a30afa81c
Stanislav Bogatyrev <realloc@clodo.ru>
2013-02-22 15:05:28 +0400

    [COOK-2464] Recipe fails if shf-server didn't create 'chef' user yet.

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -42,7 +42,7 @@ module Opscode
             else
               mode 00755
             end
-            if server
+            if server && node["etc"]["passwd"]["chef"]
               owner "chef"
               group "chef"
             else
