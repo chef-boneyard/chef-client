66a625e5a743d2345c8d8a965e34caa027b56bba
jtimberman <joshua@opscode.com>
2013-06-07 15:26:35 -0600

    [COOK-2637] - Silence expected errors from `which`

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -33,9 +33,9 @@ module Opscode
           node.recipe?("chef-server")
         else
           Chef::Log.debug("Node has Chef Server Recipe? #{node.recipe?("chef-server")}")
-          Chef::Log.debug("Node has Chef Server Executable? #{system("which chef-server > /dev/null ")}")
-          Chef::Log.debug("Node has Chef Server Ctl Executable? #{system("which chef-server-ctl > /dev/null")}")
-          node.recipe?("chef-server") || system("which chef-server > /dev/null ") || system("which chef-server-ctl > /dev/null")
+          Chef::Log.debug("Node has Chef Server Executable? #{system("which chef-server > /dev/null 2>&1")}")
+          Chef::Log.debug("Node has Chef Server Ctl Executable? #{system("which chef-server-ctl > /dev/null 2>&1")}")
+          node.recipe?("chef-server") || system("which chef-server > /dev/null 2>&1") || system("which chef-server-ctl > /dev/null 2>&1")
         end
       end
 
