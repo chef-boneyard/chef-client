7d2cd8577da6118cdba4b1e7a5f337c548b6f9ed
Steven Danna <steve@opscode.com>
2013-08-21 10:23:49 -0700

    Add tests for upstart on CentOS 6.
    Because of how the minitest handler for Chef works, we've had to
    resort to the uglier test construction used here.  Alternatively, we
    could have inspected the state of the system directly.
    
    For more information on the issue in minitest, see:
    
    https://github.com/calavera/minitest-chef-handler/issues/66

--- files/default/tests/minitest/service_test.rb
+++ files/default/tests/minitest/service_test.rb
@@ -19,6 +19,9 @@ require File.expand_path('../support/helpers', __FILE__)
 describe 'chef-client::service' do
   include Helpers::ChefClient
   it "starts the chef-client service" do
-    service("chef-client").must_be_running
+    r = Chef::Resource::Service.new("chef-client", @run_context)
+    r.provider Chef::Provider::Service::Upstart
+    current_resource = r.provider_for_action(:start).load_current_resource
+    current_resource.running.must_equal true
   end
 end
