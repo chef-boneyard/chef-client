551f95e353a7ec3d4179426d8a4f650eb779bf78
Ben Dean <ben.dean@ontariosystems.com>
2013-10-17 17:33:27 -0400

    working around schtasks weird quoting
    Apparently schtasks will turn single quotes contained in a double quoted argument into double quotes in the task.
    
    For example:
    
       /TR "cmd /c 'echo hi'"
    
    will make the created task's action run:
    
       cmd /c "echo hi"
    
    Windows is strange sometimes o_O

--- recipes/task.rb
+++ recipes/task.rb
@@ -36,9 +36,9 @@ end
 
 windows_task "chef-client" do
   run_level :highest
-  command "cmd /c \"#{node['chef_client']['ruby_bin']} #{node['chef_client']['bin']} \
+  command "cmd /c '#{node['chef_client']['ruby_bin']} #{node['chef_client']['bin']} \
   -L #{File.join(node['chef_client']['log_dir'], 'client.log')} \
-  -c #{File.join(node['chef_client']['conf_dir'], 'client.rb')} -s #{node['chef_client']['splay']} > NUL 2>&1\""
+  -c #{File.join(node['chef_client']['conf_dir'], 'client.rb')} -s #{node['chef_client']['splay']} > NUL 2>&1'"
 
   user               node['chef_client']['task']['user']
   password           node['chef_client']['task']['password']
