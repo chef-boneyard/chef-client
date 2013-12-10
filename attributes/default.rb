e788efb1005a7bd3c8a64458fd7e616e272de01f
jaxjacobs <jack.jacobs@tealium.com>
2013-12-10 12:45:32 -0800

    Added new attributes for Nagios cron timing

--- attributes/default.rb
+++ attributes/default.rb
@@ -30,6 +30,7 @@ default["chef_client"]["bin"]         = "/usr/bin/chef-client"
 default["chef_client"]["server_url"]  = "http://localhost:4000"
 default["chef_client"]["validation_client_name"] = "chef-validator"
 default["chef_client"]["cron"] = { "minute" => "0", "hour" => "*/4", "path" => nil}
+default["chef_client"]["cron"]["nagios"] = { "minute" => "*/15", "hour" => "*", "path" => nil}
 default["chef_client"]["environment"] = nil
 
 case platform
