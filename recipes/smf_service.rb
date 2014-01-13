169f52d644369e3a3a76cfdce6ccb0453850fc4d
Adrian Moisey <adrian@yola.com>
2014-01-13 13:39:53 +0200

    Remove log entries, they keep disturbing the peace on campfire

--- recipes/smf_service.rb
+++ recipes/smf_service.rb
@@ -5,7 +5,6 @@ end
 
 # libraries/helpers.rb method to DRY directory creation resources
 client_bin = find_chef_client
-log 'Found chef-client in #{client_bin}'
 node.default['chef_client']['bin'] = client_bin
 create_directories
 
