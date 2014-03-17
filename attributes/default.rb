25758e134df867f93827b15d2842296437b5097d
Timur Batyrshin <erthad@gmail.com>
2014-03-17 15:23:39 +0400

    added chef_client.cron.weekday parameter

--- attributes/default.rb
+++ attributes/default.rb
@@ -52,6 +52,7 @@ default['chef_client']['log_dir']     = '/var/log/chef'
 default['chef_client']['cron'] = {
   'minute' => '0',
   'hour' => '0,4,8,12,16,20',
+  'weekday' => '*',
   'path' => nil,
   'environment_variables' => nil,
   'log_file' => '/dev/null',
