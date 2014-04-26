3d5ffe8171d4da2b7275cd49fb303a03d8f220ba
Trevor Lauder <trevor@lauderfamily.ca>
2014-04-25 21:24:10 -0600

    COOK-4594: 'Found chef-client in' log resource
    Using the log resource for the 'Found chef-client in...' debug message causes it
    to be reported as executed on each chef-run which I don't believe is the intention
    and is confusing as it makes it appear like a resource was changed.
    
    This will still log it when run in debug mode but not as a resource.

--- recipes/launchd_service.rb
+++ recipes/launchd_service.rb
@@ -7,9 +7,7 @@ require 'chef/version_constraint'
 
 # libraries/helpers.rb method to DRY directory creation resources
 client_bin = find_chef_client
-log "Found chef-client in #{client_bin}" do
-  level :debug
-end
+Chef::Log.debug("Found chef-client in #{client_bin}")
 node.default['chef_client']['bin'] = client_bin
 create_directories
 
