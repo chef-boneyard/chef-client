a33edcc40d75422940656f1b8e896955bb95c30e
Christopher Webber <cwebber@getchef.com>
2014-08-13 14:48:51 -0700

    Move cook-2086 test to chefspec

---
+++ spec/unit/cron_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'chef-client::cron' do
+
+  let(:chef_run) do
+    ChefSpec::Runner.new.converge(described_recipe)
+  end
+
+  context 'Custom Attributes' do
+
+    let(:chef_run) do
+      ChefSpec::Runner.new do |node|
+        node.set['chef_client']['cron']['environment_variables'] = 'FOO=BAR'
+      end.converge(described_recipe)
+    end
+
+    it 'sets the FOO=BAR environment variable' do
+      expect(chef_run).to create_cron('chef-client') \
+        .with(command: %r{/bin/sleep \d+; FOO=BAR /usr/bin/chef-client > /dev/null 2>&1})
+    end
+
+  end
+
+end
