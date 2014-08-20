d2d15c15586ae13caf210ed6c7effe3d45e9281d
Christopher Webber <cwebber@getchef.com>
2014-08-19 18:30:08 -0700

    Rip out mini-test handler

--- test/cookbooks/chef-client_test/recipes/config.rb
+++
@@ -1,6 +0,0 @@
-#
-# Cookbook Name:: chef-client_test
-# Recipe:: config
-#
-# This recipe is included in the suite so the minitest-chef-handler
-# tests get run.
--- test/cookbooks/chef-client_test/recipes/cook-2086.rb
+++
@@ -1,6 +0,0 @@
-#
-# Cookbook Name:: chef-client
-# Recipe:: cook-2086
-#
-# This recipe is included in the suite so the minitest-chef-handler
-# tests get run.
