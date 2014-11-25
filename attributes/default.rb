1e098be604bbf35c59b3bb66ec6d77d9c624a8b7
Gregoire Seux <g.seux@criteo.com>
2014-11-25 17:57:55 +0100

    Default log_file to client.log and fix redhat init file
    This effectively drops support for logging configured via sysconfig
    which is in conflict with log_{dir,file} attribute.

--- attributes/default.rb
+++ attributes/default.rb
@@ -36,7 +36,7 @@ if Chef::Config.has_key?(:client_fork)
 end
 
 # By default, we don't have a log file, as we log to STDOUT
-default['chef_client']['log_file']    = nil
+default['chef_client']['log_file']    = 'client.log'
 default['chef_client']['interval']    = '1800'
 default['chef_client']['splay']       = '300'
 default['chef_client']['conf_dir']    = '/etc/chef'
