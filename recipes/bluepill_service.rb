c81b27c6a89e58c154683004eb6305a687058d33
Simon Detheridge <simon@widgit.com>
2015-05-29 12:14:11 +0100

    Replace missing root_group with node var in service recipes

--- recipes/bluepill_service.rb
+++ recipes/bluepill_service.rb
@@ -9,7 +9,7 @@ Chef::Log.debug("Found chef-client in #{client_bin}")
 node.default['chef_client']['bin'] = client_bin
 create_directories
 
-group = root_group
+group = node['root_group']
 
 directory node['chef_client']['run_path'] do
   recursive true
