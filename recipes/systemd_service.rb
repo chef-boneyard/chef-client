5d9f52917819426f28b7f539b363c159f73c0424
Grégoire Seux <g.seux@criteo.com>
2016-05-04 18:55:44 +0200

    Allow to use systemd timer instead of chef-client daemon mode
    This patches adds an attribute to configure a systemd timer starting the
    chef-client.service.
    chef-client.service is modified in this case not to run in daemon mode.
    
    Options `interval` and `splay` are respected.
    
    Change-Id: I8a47852f1b045f59e29b8c4af4a6c0aa3c25c40c
    Signed-off-by: Grégoire Seux <g.seux@criteo.com>

--- recipes/systemd_service.rb
+++ recipes/systemd_service.rb
@@ -15,20 +15,60 @@ dist_dir, conf_dir, env_file = value_for_platform_family(
   ['debian'] => ['debian', 'default', 'chef-client']
 )
 
+timer = node['chef_client']['systemd']['timer']
+
+exec_options = if timer
+                 '-c $CONFIG $OPTIONS'
+               else
+                 '-c $CONFIG -i $INTERVAL -s $SPLAY $OPTIONS'
+               end
+
 template '/etc/systemd/system/chef-client.service' do
   source 'systemd/chef-client.service.erb'
   mode '644'
-  variables(client_bin: client_bin, sysconfig_file: "/etc/#{conf_dir}/#{env_file}")
-  notifies :restart, 'service[chef-client]', :delayed
+  variables(
+    client_bin: client_bin,
+    sysconfig_file: "/etc/#{conf_dir}/#{env_file}",
+    type: (timer ? 'oneshot' : 'simple'),
+    exec_options: exec_options,
+    restart_mode: (timer ? nil : 'on-failure')
+  )
+  notifies :restart, 'service[chef-client]', :delayed unless node['chef_client']['systemd']['timer']
 end
 
 template "/etc/#{conf_dir}/#{env_file}" do
   source "#{dist_dir}/#{conf_dir}/chef-client.erb"
   mode '644'
-  notifies :restart, 'service[chef-client]', :delayed
+  notifies :restart, 'service[chef-client]', :delayed unless node['chef_client']['systemd']['timer']
 end
+
+service_actions = [:enable]
+service_actions << :start unless node['chef_client']['systemd']['timer']
 
 service 'chef-client' do
   supports status: true, restart: true
-  action [:enable, :start]
+  action service_actions
+end
+
+systemd_unit 'chef-client.timer' do
+  content <<-EOH.gsub('^  ', '')
+  [Unit]
+  Description=chef-client periodic run
+
+  [Install]
+  WantedBy=timers.target
+
+  [Timer]
+  OnBootSec=1min
+  OnUnitActiveSec=#{node['chef_client']['interval']}sec
+  # AccuracySec is available on all versions of systemd
+  # (whereas RandomizedDelaySec > 229) and will avoid the herd effect
+  AccuracySec=#{node['chef_client']['splay']}sec
+  EOH
+  enabled timer
+  if timer
+    action [:create, :enable]
+  else
+    action [:disable, :delete]
+  end
 end
