b66b37a074f40ec2aedc05ccab8c0cffcd25ddc6
Tim Smith <tsmith@chef.io>
2016-05-18 18:52:18 -0700

    Add more service specs
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/service_spec.rb
+++ spec/unit/service_spec.rb
@@ -41,28 +41,35 @@ describe 'chef-client::service' do
   end
 
   context 'Fedora' do
-    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'fedora', version: '20').converge(described_recipe) }
+    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'fedora', version: '23').converge(described_recipe) }
     it 'should use the systemd service' do
       expect(chef_run).to include_recipe('chef-client::systemd_service')
     end
   end
 
-  context 'FreeBSD' do
+  context 'FreeBSD 9' do
     let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'freebsd', version: '9.2').converge(described_recipe) }
     it 'should use the bsd service' do
       expect(chef_run).to include_recipe('chef-client::bsd_service')
     end
   end
 
+  context 'FreeBSD 10' do
+    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'freebsd', version: '10.3').converge(described_recipe) }
+    it 'should use the bsd service' do
+      expect(chef_run).to include_recipe('chef-client::bsd_service')
+    end
+  end
+
   context 'Mac OS X' do
-    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'mac_os_x', version: '10.8.2').converge(described_recipe) }
+    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'mac_os_x', version: '10.11.1').converge(described_recipe) }
     it 'should use the launchd service' do
       expect(chef_run).to include_recipe('chef-client::launchd_service')
     end
   end
 
   context 'OpenSuSE' do
-    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'opensuse', version: '13.1').converge(described_recipe) }
+    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'opensuse', version: '13.2').converge(described_recipe) }
     it 'should use the init service' do
       expect(chef_run).to include_recipe('chef-client::init_service')
     end
