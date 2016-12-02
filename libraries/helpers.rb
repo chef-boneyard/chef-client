c5c2eacbd1c3319af02560ca5f9e6be217538072
Tom Duffield <tom@chef.io>
2016-12-02 15:51:18 -0600

    Address lint errors
    Signed-off-by: Tom Duffield <tom@chef.io>

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -51,7 +51,7 @@ module Opscode
           rescue Chef::Exceptions::ResourceNotFound
             directory node['chef_client'][dir] do
               recursive true
-              mode "755" if dir == 'log_dir'
+              mode '755' if dir == 'log_dir'
               owner d_owner
               group node['root_group']
             end
