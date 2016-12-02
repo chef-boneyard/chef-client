ad66fa122d11b5d65490de5a51bd59e0cb442b82
Tom Duffield <tom@chef.io>
2016-12-02 15:17:40 -0600

    Use first-class Inspec service tester
    Signed-off-by: Tom Duffield <tom@chef.io>

--- test/integration/service_runit/service_runit_spec.rb
+++ test/integration/service_runit/service_runit_spec.rb
@@ -5,3 +5,9 @@ end
 describe file('/etc/service/chef-client/run') do
   it { should be_file }
 end
+
+describe service('chef-client') do
+  it { should be_enabled }
+  it { should be_installed }
+  it { should be_running }
+end
