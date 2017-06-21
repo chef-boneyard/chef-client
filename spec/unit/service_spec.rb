8ff4a7069bd38bfb52c4288169d223205c456cc8
Tim Smith <tsmith@chef.io>
2017-06-21 12:46:47 -0700

    Update specs for new fauxhai data
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/service_spec.rb
+++ spec/unit/service_spec.rb
@@ -29,7 +29,7 @@ describe 'chef-client::service' do
   end
 
   context 'Fedora' do
-    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'fedora', version: '24').converge(described_recipe) }
+    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'fedora', version: '25').converge(described_recipe) }
     it 'should use the systemd service' do
       expect(chef_run).to include_recipe('chef-client::systemd_service')
     end
@@ -50,7 +50,7 @@ describe 'chef-client::service' do
   end
 
   context 'Mac OS X' do
-    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'mac_os_x', version: '10.11.1').converge(described_recipe) }
+    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'mac_os_x', version: '10.12').converge(described_recipe) }
     it 'should use the launchd service' do
       expect(chef_run).to include_recipe('chef-client::launchd_service')
     end
