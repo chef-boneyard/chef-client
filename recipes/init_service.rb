16b57322ef1e3946a71cba53d03856b27d4bcbac
Tim Smith <tsmith@chef.io>
2017-08-04 17:08:18 -0700

    Use the full file modes in the init service recipe
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/init_service.rb
+++ recipes/init_service.rb
@@ -17,14 +17,14 @@ dist_dir, conf_dir = value_for_platform_family(
 
 template '/etc/init.d/chef-client' do
   source "#{dist_dir}/init.d/chef-client.erb"
-  mode '755'
+  mode '0755'
   variables client_bin: client_bin
   notifies :restart, 'service[chef-client]', :delayed
 end
 
 template "/etc/#{conf_dir}/chef-client" do
   source "#{dist_dir}/#{conf_dir}/chef-client.erb"
-  mode '644'
+  mode '0644'
   notifies :restart, 'service[chef-client]', :delayed
 end
 
