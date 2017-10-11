f9a50e4d4bd7b38218964894e6661407687507c3
dheerajd-msys <dheeraj.dubey@msystechnologies.com>
2017-10-11 19:31:10 +0530

    converge if changed
    Signed-off-by: dheerajd-msys <dheeraj.dubey@msystechnologies.com>

--- resources/scheduled_task.rb
+++ resources/scheduled_task.rb
@@ -19,16 +19,54 @@
 
 resource_name :chef_client_scheduled_task
 
-property :user, String, default: 'System'
+property :user, String, default: node['chef_client']['task']['user']
 property :password, String
-property :frequency, String, default: 'minute'
-property :frequency_modifier, Integer, default: 30
-property :start_time, String
-property :splay, [Integer, String], default: 300
+property :frequency, String, default: node['chef_client']['task']['frequency']
+property :frequency_modifier, Integer, default: node['chef_client']['task']['frequency_modifier']
+property :start_time, String, default: node['chef_client']['task']['start_time']
+property :splay, [Integer, String], default: node['chef_client']['splay']
 property :config_directory, String, default: 'C:/chef'
 property :log_directory, String, default: lazy { |r| "#{r.config_directory}/log" }
 property :chef_binary_path, String, default: 'C:/opscode/chef/bin/chef-client'
 property :daemon_options, Array, default: []
+property :task_name, String, default: node['chef-client']['task']['task_name']
+
+def load_task_hash(task_name)
+  Chef::Log.debug 'Looking for existing tasks'
+
+  # we use powershell_out here instead of powershell_out! because a failure implies that the task does not exist
+  task_script = <<-EOH
+    [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
+    schtasks /Query /FO LIST /V /TN \"#{task_name}\"
+  EOH
+  output = powershell_out(task_script).stdout.force_encoding('UTF-8')
+  if output.empty?
+    task = false
+  else
+    task = {}
+
+    output.split("\n").map! { |line| line.split(':', 2).map!(&:strip) }.each do |field|
+      if field.is_a?(Array) && field[0].respond_to?(:to_sym)
+        task[field[0].gsub(/\s+/, '').to_sym] = field[1]
+      end
+    end
+  end
+
+  task
+end
+
+load_current_value do |desired|
+  pathed_task_name = desired.task_name.start_with?('\\') ? desired.task_name : "\\#{desired.task_name}"
+
+  task_hash = load_task_hash pathed_task_name
+
+  task_name pathed_task_name
+  if task_hash.respond_to?(:[]) && task_hash[:TaskName] == pathed_task_name
+    user task_hash[:RunAsUser]
+    start_time task_hash[:StartTime]
+    frequency task_hash[:ScheduleType]
+  end
+end
 
 action :add do
   create_chef_directories
@@ -42,7 +80,7 @@ action :add do
   # Add custom options
   client_cmd << " #{new_resource.daemon_options.join(' ')}" if new_resource.daemon_options.any?
 
-  start_time = new_resource.frequency == 'minute' ? (Time.now + 60 * new_resource.frequency_modifier).strftime('%H:%M') : nil
+  #start_time = new_resource.frequency == 'minute' ? (Time.now + 60 * new_resource.frequency_modifier).strftime('%H:%M') : nil
   windows_task 'chef-client' do
     run_level :highest
     command "cmd /c \"#{client_cmd}\""
@@ -51,12 +89,35 @@ action :add do
     password           new_resource.password
     frequency          new_resource.frequency.to_sym
     frequency_modifier new_resource.frequency_modifier
-    start_time         new_resource.start_time || start_time
+    start_time         new_resource.start_time
   end
 end
 
 action :remove do
   windows_task 'chef-client' do
     action :delete
+  end
+end
+
+action :create do
+  converge_if_changed :user do
+    windows_task 'chef-client' do
+      user new_resource.user
+    end
+  end
+  converge_if_changed :frequency do
+    windows_task 'chef-client' do
+      frequency new_resource.frequency
+    end
+  end
+  converge_if_changed :frequency_modifier do
+    windows_task 'chef-client' do
+      frequency_modifier new_resource.frequency_modifier
+    end
+  end
+  converge_if_changed :start_time do
+    windows_task 'chef-client' do
+      start_time new_resource.start_time
+    end
   end
 end
