ba4b75eba6e69a97d37a0e4d2290c634808e824f
Tim Smith <tsmith@chef.io>
2017-10-23 13:32:01 -0700

    Add scheduled_task inspec test
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/task/task_spec.rb
+++ test/integration/task/task_spec.rb
@@ -6,3 +6,9 @@ describe file('C:/chef/client.rb') do
   its('content') { should match(/ohai.disabled_plugins = \["Mdadm"\]/) }
   its('content') { should match(%r{ohai.plugin_path << "/tmp/kitchen/ohai/plugins"}) }
 end
+
+describe windows_task('chef-client') do
+  it { should be_enabled }
+  its('run_as_user') { should eq 'SYSTEM' }
+  its('task_to_run') { should match 'cmd /c C:/opscode/chef/bin/chef-client -L C:/chef/log/client.log -c C:/chef/client.rb -s 300' }
+end
