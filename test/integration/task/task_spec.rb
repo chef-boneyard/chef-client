e7c9df7410412035971e23631bafae3c603c0362
Tim Smith <tsmith@chef.io>
2017-10-23 14:16:03 -0700

    Skip the windows_task inspec test on 2k8r2
    We need PS 3.0+ and we don't have it there
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/task/task_spec.rb
+++ test/integration/task/task_spec.rb
@@ -7,8 +7,11 @@ describe file('C:/chef/client.rb') do
   its('content') { should match(%r{ohai.plugin_path << "/tmp/kitchen/ohai/plugins"}) }
 end
 
-describe windows_task('chef-client') do
-  it { should be_enabled }
-  its('run_as_user') { should eq 'SYSTEM' }
-  its('task_to_run') { should match 'cmd /c C:/opscode/chef/bin/chef-client -L C:/chef/log/client.log -c C:/chef/client.rb -s 300' }
+# the inspec resource requires PS 3.0+ and 2k8r2 only has PS 2.0 by default
+unless os.release.to_f == 6.1
+  describe windows_task('chef-client') do
+    it { should be_enabled }
+    its('run_as_user') { should eq 'SYSTEM' }
+    its('task_to_run') { should match 'cmd /c C:/opscode/chef/bin/chef-client -L C:/chef/log/client.log -c C:/chef/client.rb -s 300' }
+  end
 end
