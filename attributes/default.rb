fa855a0126426c2cf4ea3ba2a9ad603f08bd6c39
Tim Smith <tsmith@chef.io>
2017-11-14 12:12:35 -0800

    Add default['chef_client']['task']['start_time']  to the attributes file
    Even though it's nil we should list it so it's clear this is a config item
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -71,6 +71,7 @@ default['chef_client']['task']['frequency'] = 'minute'
 default['chef_client']['task']['frequency_modifier'] = node['chef_client']['interval'].to_i / 60
 default['chef_client']['task']['user'] = 'SYSTEM'
 default['chef_client']['task']['password'] = nil # Password is only required for none system users
+default['chef_client']['task']['start_time'] = nil
 
 default['chef_client']['load_gems'] = {}
 
