c6ccdae3968958013134ac994684f329266c3536
Tim Smith <tsmith@chef.io>
2017-11-14 13:55:33 -0800

    Test the start_date functionality
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/test/recipes/task.rb
+++ test/cookbooks/test/recipes/task.rb
@@ -1,5 +1,6 @@
 node.override['chef_client']['interval'] = 900
 node.override['chef_client']['task']['frequency_modifier'] = '31' # this is a string to test that "typo"
+node.override['chef_client']['task']['start_date'] = Time.now.strftime('%m/%m/%Y')
 
 include_recipe 'test::config'
 include_recipe 'chef-client::task'
