fbd299334b080440faeda4445443b0783963a73c
dheerajd-msys <dheeraj.dubey@msystechnologies.com>
2018-02-05 18:57:16 +0530

    update start_date format attribute
    Signed-off-by: dheerajd-msys <dheeraj.dubey@msystechnologies.com>

--- test/cookbooks/test/recipes/task.rb
+++ test/cookbooks/test/recipes/task.rb
@@ -1,6 +1,6 @@
 node.override['chef_client']['interval'] = 900
 node.override['chef_client']['task']['frequency_modifier'] = '31' # this is a string to test that "typo"
-node.override['chef_client']['task']['start_date'] = Time.now.strftime('%m/%m/%Y')
+node.override['chef_client']['task']['start_date'] = Time.now.strftime('%m/%d/%Y')
 
 include_recipe 'test::config'
 include_recipe 'chef-client::task'
