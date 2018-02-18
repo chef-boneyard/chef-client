1b072371df4cedebf52c9efbd7f603ab4e29955e
Tim Smith <tsmith@chef.io>
2018-02-18 09:33:33 -0800

    Add more specs for SLES 11 init script
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/init_service_spec.rb
+++ spec/unit/init_service_spec.rb
@@ -40,7 +40,7 @@ describe 'chef-client::init_service' do
 
     it 'should not include OPTIONS in chef-client start command' do
       expect(chef_run).not_to render_file("/etc/#{sles[:conf_dir]}/chef-client") \
-        .with_content(%r{"\$OPTIONS"})
+        .with_content(/"\$OPTIONS"/)
     end
   end
 
@@ -51,14 +51,44 @@ describe 'chef-client::init_service' do
       end.converge(described_recipe)
     end
 
+    it 'should set OPTIONS to "-E client-args"' do
+      expect(chef_run).to render_file("/etc/#{sles[:conf_dir]}/chef-client") \
+        .with_content(/-E client-args/)
+    end
+
     it 'should include OPTIONS in chef-client start command' do
       expect(chef_run).to render_file("/etc/#{sles[:conf_dir]}/chef-client") \
-        .with_content(%r{"\$OPTIONS"})
+        .with_content(/"\$OPTIONS"/)
     end
 
     it 'should set PIDFILE to /var/run/chef/client.pid' do
       expect(chef_run).to render_file("/etc/#{sles[:conf_dir]}/chef-client") \
         .with_content(%r{PIDFILE-/var/run/chef/client.pid})
+    end
+
+    it 'should set CONFIG to /etc/chef/client.rb' do
+      expect(chef_run).to render_file("/etc/#{sles[:conf_dir]}/chef-client") \
+        .with_content(%r{CONFIG-/etc/chef/client.rb})
+    end
+
+    it 'should set INTERVAL to /etc/chef/client.rb' do
+      expect(chef_run).to render_file("/etc/#{sles[:conf_dir]}/chef-client") \
+        .with_content(/INTERVAL-1800/)
+    end
+
+    it 'should set SPLAY to /etc/chef/client.rb' do
+      expect(chef_run).to render_file("/etc/#{sles[:conf_dir]}/chef-client") \
+        .with_content(/SPLAY-300/)
+    end
+
+    it 'should set LOCKFILE to /etc/chef/client.rb' do
+      expect(chef_run).to render_file("/etc/#{sles[:conf_dir]}/chef-client") \
+        .with_content(%r{LOCKFILE-/var/lock/subsys/chef-client})
+    end
+
+    it 'should include sysconfig file at /etc/sysconfig/chef-client' do
+      expect(chef_run).to render_file("/etc/#{sles[:conf_dir]}/chef-client") \
+        .with_content(%r{/etc/sysconfig/chef-client})
     end
   end
 end
