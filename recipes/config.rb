537a5a2a2b9480302bae55a8c2bf8ba4a5afd1cd
Jason Barnett <jason.w.barnett@gmail.com>
2018-11-27 15:57:39 -0500

    Prepend :: to File call
    Developer's Certificate of Origin 1.1
    
    By making a contribution to this project, I certify that:
    
    (a) The contribution was created in whole or in part by me and I
        have the right to submit it under the open source license
        indicated in the file; or
    
    (b) The contribution is based upon previous work that, to the
        best of my knowledge, is covered under an appropriate open
        source license and I have the right under that license to
        submit that work with modifications, whether created in whole
        or in part by me, under the same open source license (unless
        I am permitted to submit under a different license), as
        Indicated in the file; or
    
    (c) The contribution was provided directly to me by some other
        person who certified (a), (b) or (c) and I have not modified
        it.
    
    (d) I understand and agree that this project and the contribution
        are public and that a record of the contribution (including
        all personal information I submit with it, including my
        sign-off) is maintained indefinitely and may be redistributed
        consistent with this project or the open source license(s)
        involved.
    
    Signed-off-by: Jason Barnett <jason.w.barnett@gmail.com>

--- recipes/config.rb
+++ recipes/config.rb
@@ -28,7 +28,7 @@ end
 # chef_node_name = Chef::Config[:node_name] == node['fqdn'] ? false : Chef::Config[:node_name]
 
 if node['chef_client']['log_file'].is_a?(String)
-  log_path = File.join(node['chef_client']['log_dir'], node['chef_client']['log_file'])
+  log_path = ::File.join(node['chef_client']['log_dir'], node['chef_client']['log_file'])
   node.default['chef_client']['config']['log_location'] = log_path
 
   if node['os'] == 'linux'
