dd6d5e857aa4201150528ba232cf41f3d3c5d1c1
Nathan Williams <nathan@teamtreehouse.com>
2019-01-29 16:06:26 -0800

    fix systemd timer activation
    Signed-off-by: Nathan Williams <nathan@teamtreehouse.com>

---
+++ test/integration/timer_systemd/timer_systemd_spec.rb
@@ -0,0 +1,14 @@
+describe systemd_service('chef-client.timer') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe command('systemctl show -p Triggers chef-client.timer') do
+  its('stdout') { should match 'Triggers=chef-client.service' }
+end
+
+describe file('/etc/systemd/system/chef-client.timer') do
+  its('content') { should match 'OnBootSec = 1min' }
+  its('content') { should match 'OnUnitInactiveSec = 1800sec' }
+  its('content') { should match 'RandomizedDelaySec = 300sec' }
+end
