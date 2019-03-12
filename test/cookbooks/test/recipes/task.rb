62142086e5192fb845d9743f7025cc4ad38c1a14
Graham Davison <gdavison@paybyphone.com>
2019-03-11 17:11:48 -0700

    Added `onstart` scheduled task to test recipe and updated `chef_client_scheduled_task` resource
    Signed-off-by: Graham Davison <gdavison@paybyphone.com>

--- test/cookbooks/test/recipes/task.rb
+++ test/cookbooks/test/recipes/task.rb
@@ -5,3 +5,14 @@ node.override['chef_client']['task']['start_date'] = Time.now.strftime('%m/%d/%Y
 include_recipe 'test::config'
 include_recipe 'chef-client::task'
 include_recipe 'chef-client::delete_validation'
+
+chef_client_scheduled_task 'Chef Client on start' do
+  user             node['chef_client']['task']['user']
+  password         node['chef_client']['task']['password']
+  frequency        'onstart'
+  config_directory node['chef_client']['conf_dir']
+  log_directory    node['chef_client']['log_dir']
+  chef_binary_path node['chef_client']['bin']
+  daemon_options   node['chef_client']['daemon_options']
+  task_name        "#{node['chef_client']['task']['name']}-onstart"
+end
