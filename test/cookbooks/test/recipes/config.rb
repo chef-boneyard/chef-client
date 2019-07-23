60f7fea16b44cb2b6752c190427518216ccf81da
Tim Smith <tsmith84@gmail.com>
2019-07-23 14:12:12 -0700

    Remove the license test suite and do the same tests in config
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/test/recipes/config.rb
+++ test/cookbooks/test/recipes/config.rb
@@ -1,3 +1,4 @@
 node.override['ohai']['disabled_plugins'] = ['Mdadm']
 node.override['ohai']['plugin_path'] = '/tmp/kitchen/ohai/plugins'
+node.override['chef_client']['chef_license'] = 'accept-no-persit'
 include_recipe 'chef-client::config'
--- test/integration/config/client_rb_spec.rb
+++ test/integration/config/client_rb_spec.rb
@@ -11,5 +11,5 @@ end
 describe file(config) do
   its('content') { should match(/ohai.disabled_plugins = \["Mdadm"\]/) }
   its('content') { should match(%r{ohai.plugin_path << "/tmp/kitchen/ohai/plugins"}) }
-  its('content') { should_not match(/chef_license/) }
+  its('content') { should match(/chef_license "accept-no-persist"/) }
 end
