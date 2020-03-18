83f522a1651645c375137dfac9cb5da1cdce3633
Tim Smith <tsmith84@gmail.com>
2020-03-17 21:23:03 -0700

    Add chef_client_cron resource
    Follows a similar pattern to the legacy attributes, but this lets you more easily
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ test/cookbooks/test/recipes/cron_resource.rb
@@ -0,0 +1,1 @@
+chef_client_cron 'schedule chef-client to run as cron job'
--- test/integration/cron_daemon/cron_spec.rb
+++
@@ -1,4 +0,0 @@
-describe command('crontab -u root -l') do
-  its(:stdout) { should match(/chef-client/) }
-  its(:stdout) { should match(/--run-lock-timeout 0/) }
-end
