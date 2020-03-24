5829a1857104dbe4fbb3c11109d2b14b8280ef5d
Tim Smith <tsmith84@gmail.com>
2020-03-24 14:33:41 -0700

    Fix tests cron tests on Fedora
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/test/recipes/cron.rb
+++ test/cookbooks/test/recipes/cron.rb
@@ -1,5 +1,6 @@
 apt_update 'update'
 include_recipe 'test::config'
 include_recipe 'cron::default'
+package 'crontabs' if platform?('fedora') # ensures we actually have the /etc/cron.d dir
 include_recipe 'chef-client::cron'
 include_recipe 'chef-client::delete_validation'
