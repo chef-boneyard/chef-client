a6e9eec7accff841996ebfea37e8c71cd6956e9c
Tim Smith <tsmith84@gmail.com>
2020-03-24 10:43:57 -0700

    Update cron resource with more fixes
    1) properly handle environment values by using cron_d natively
    2) Add support for setting day/month cron values
    3) Rename env_vars to environment to match cron
    4) Ensure that splay is a positive number
    5) Add a comment property
    6) Create the log directory if it doesn't exist
    7) Fix some formatting issues in the cron_command method
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/cron.rb
+++ resources/cron.rb
@@ -23,13 +23,19 @@ property :user, String, default: 'root'
 
 property :minute, [String, Integer], default: '0,30'
 property :hour, [String, Integer], default: '*'
+property :day, [Integer, String], default: '*'
+property :month, [Integer, String], default: '*'
 property :weekday, [String, Integer], default: '*'
 property :mailto, String
 
 property :job_name, String, default: 'chef-client'
-property :splay, [Integer, String], default: 300
+property :splay, [Integer, String], default: 300,
+                                    coerce: proc { |x| Integer(x) },
+                                    callbacks: { 'should be a positive number' => proc { |v| v > 0 } }
 
-property :env_vars, Hash
+property :environment, Hash, default: lazy { {} }
+
+property :comment, String
 
 property :config_directory, String, default: '/etc/chef'
 property :log_directory, String, default: lazy { platform?('mac_os_x') ? '/Library/Logs/Chef' : '/var/log/chef' }
@@ -39,13 +45,25 @@ property :chef_binary_path, String, default: '/opt/chef/bin/chef-client'
 property :daemon_options, Array, default: []
 
 action :add do
+  unless ::Dir.exist?(new_resource.log_directory)
+    directory new_resource.log_directory do
+      owner new_resource.user
+      mode '0640'
+      recursive true
+    end
+  end
+
   cron_d new_resource.job_name do
-    minute  new_resource.minute
-    hour    new_resource.hour
-    weekday new_resource.weekday
-    mailto  new_resource.mailto if new_resource.mailto
-    user    new_resource.user
-    command cron_command
+    minute      new_resource.minute
+    hour        new_resource.hour
+    day         new_resource.day
+    weekday     new_resource.weekday
+    month       new_resource.month
+    environment new_resource.environment
+    mailto      new_resource.mailto if new_resource.mailto
+    user        new_resource.user
+    comment     new_resource.comment if new_resource.comment
+    command     cron_command
   end
 end
 
@@ -59,10 +77,10 @@ action_class do
   def cron_command
     cmd = ''
     cmd << "/bin/sleep #{splay_sleep_time(new_resource.splay)}; "
-    cmd << "#{new_resource.env_vars} " if new_resource.env_vars
-    cmd << "#{new_resource.chef_binary_path} #{new_resource.daemon_options.join(' ')}"
-    cmd << " #{new_resource.append_log_file ? '>>' : '>'} #{::File.join(new_resource.log_directory, new_resource.log_file_name)} 2>&1"
-    cmd << ' || echo "Chef Infra Client execution failed"' if new_resource.mailto
+    cmd << "#{new_resource.chef_binary_path} "
+    cmd << "#{new_resource.daemon_options.join(' ')} " unless new_resource.daemon_options.empty?
+    cmd << "#{new_resource.append_log_file ? '>>' : '>'} #{::File.join(new_resource.log_directory, new_resource.log_file_name)} 2>&1"
+    cmd << " || echo \"#{Chef::Dist::PRODUCT} execution failed\"" if new_resource.mailto
     cmd
   end
 end
