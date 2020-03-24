06d3454f21f114c6af3deeed6c78efc7e8047373
Tim Smith <tsmith84@gmail.com>
2020-03-24 14:33:29 -0700

    Delete unused tests
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/service_upstart/service_upstart_spec.rb
+++
@@ -1,9 +0,0 @@
-describe processes('chef-client') do
-  it { should exist }
-end
-
-describe service('chef-client') do
-  it { should be_enabled }
-  it { should be_installed }
-  it { should be_running }
-end
