cfd60b127fe93478e9692e3eff7dad920204ecd8
Tim Smith <tsmith84@gmail.com>
2020-04-07 19:10:11 -0700

    Accept the license in the systemd_timer test
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/test/recipes/systemd_timer_resource.rb
+++ test/cookbooks/test/recipes/systemd_timer_resource.rb
@@ -1,3 +1,4 @@
 chef_client_systemd_timer 'schedule chef-client to run as cron job' do
-  daemon_options ['--run-lock-timeout 0', '--chef-license accept']
+  accept_chef_license true
+  daemon_options ['--run-lock-timeout 0']
 end
